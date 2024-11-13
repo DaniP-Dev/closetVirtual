16bcfd578892c80193030771d3d5be3a462838db
Daniel Perez <dperezg23@outlook.com>
2024-11-13 09:35:11 -0500

    prueba para responsibidad

---
+++ src/app/componentes/Navbar.js
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <div className="w-full bg-blue-600 p-4">
+            <h1 className="text-white text-center">Navbar</h1>
+        </div>
+    )
+}
+
+export default Navbar;
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
-import Image from "next/image";
+import Navbar from "./componentes/Navbar";
 
 export default function Home() {
   return (
-    <div className=" bg-red-500">
-      Hola
+    <div className="flex flex-col space-y-4">
+      <Navbar />
+      <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4">
+        <Navbar />
+        <Navbar />
+      </div>
+      <Navbar />
     </div>
   );
 }
