a35a5f35a28013a0ed1421a956d84ffd3284003c
Daniel Perez <dperezg23@outlook.com>
2024-11-13 10:55:41 -0500

    ajuste de paginas horizontales

--- src/app/componentes/Navbar.js
+++ src/app/componentes/Navbar.js
@@ -1,9 +1,11 @@
+import React from 'react';
+
 const Navbar = () => {
     return (
-        <div className="w-full bg-blue-600 p-4">
+        <div className="w-full bg-blue-600 p-4 fixed top-0 left-0">
             <h1 className="text-white text-center">Navbar</h1>
         </div>
-    )
-}
+    );
+};
 
 export default Navbar;
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,15 @@
 import Navbar from "./componentes/Navbar";
+import Body from "./componentes/Body";
 
 export default function Home() {
   return (
-    <div className="flex flex-col space-y-4">
+    <div className="flex flex-col h-screen overflow-hidden">
       <Navbar />
-      <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4">
-        <Navbar />
-        <Navbar />
+      <div className="snap-y snap-mandatory h-full overflow-y-scroll">
+        <Body color="bg-red-500" />
+        <Body color="bg-green-500" />
+        <Body color="bg-blue-500" />
       </div>
-      <Navbar />
     </div>
   );
 }
